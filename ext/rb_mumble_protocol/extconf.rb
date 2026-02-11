67a8cc3901a880e501f9b8b869578ddaba2e2b20
Mikhail Odebe <derpiranha@gmail.com>
2026-02-11 23:27:03 +0500

    compiling rust in release profile

--- ext/rb_mumble_protocol/extconf.rb
+++ ext/rb_mumble_protocol/extconf.rb
@@ -3,4 +3,6 @@
 require "mkmf"
 require "rb_sys/mkmf"
 
-create_rust_makefile("rb_mumble_protocol/rb_mumble_protocol")
+create_rust_makefile("rb_mumble_protocol/rb_mumble_protocol") do |config|
+  config.profile = :release
+end
